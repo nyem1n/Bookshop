f4c1dcf6b1d1f73e54a8fab85c1de896ad187a5b
신혜민 <shinhyemin225@gmail.com>
2025-03-11 18:46:18 +0900

    Feat: 유저 컨트롤러 추가

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const conn = require('../mariadb');
+const {
+    join, 
+    login, 
+    passwordResetRequest, 
+    passwordReset
+} = require('../controller/UserController')
 
-router.use(express.json())
+router.use(express.json());
 
-router.post('/join', (req, res) => {
-    res.json('회원 가입')
-});
-
-router.post('/login', (req, res) => {
-    res.json('로그인')
-});
-
-router.post('/reset', (req, res) => {
-    res.json('비밀번호 초기화 요청')
-});
-
-router.put('/reset', (req, res) => {
-    res.json('비밀번호 초기화')
-});
+router.post('/join', join);
+router.post('/login', login);
+router.post('/reset', passwordResetRequest);
+router.put('/reset', passwordReset);
 
 module.exports = router;
