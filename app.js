7822d80a8ee30b1d8ac70630a3cf49224611e0d3
신혜민 <shinhyemin225@gmail.com>
2025-03-12 15:16:17 +0900

    Feat: book, category 추가

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ const userRouter = require('./routes/users');
 const bookRouter = require('./routes/books');
 const likeRouter = require('./routes/likes');
 const cartRouter = require('./routes/carts');
+const orderRouter = require('./routes/orders');
+const categoryRouter = require('./routes/category')
 
 app.use('/users', userRouter);
 app.use('/books', bookRouter);
+app.use('/category', categoryRouter);
 app.use('/likes', likeRouter);
 app.use('/carts', cartRouter);
+app.use('/orders', orderRouter);
