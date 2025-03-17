37dc1fa1473d828cf6c01efa36f6547939932e4b
신혜민 <shinhyemin225@gmail.com>
2025-03-17 16:37:55 +0900

    Feat: 장바구니 기능 추가

--- routes/carts.js
+++ routes/carts.js
@@ -1,19 +1,14 @@
 const express = require('express');
+const { addToCart, getCartItems, removeCartItem } = require('../controller/CartController');
 const router = express.Router();
+
 
 router.use(express.json())
 
-router.post('/', (req, res) => {
-    res.json('장바구니 담기')
-});
+router.post('/',addToCart);
+router.get('/', getCartItems);
+router.delete('/:id', removeCartItem);
 
-router.get('/', (req, res) => {
-    res.json('장바구니 조회')
-});
-
-router.delete('/:id', (req, res) => {
-    res.json('장바구니 도서 삭제')
-});
 
 // 장바구니에서 선택한 주문 예상 상품 목록 조회
 // router.get('/carts', (req, res) => {
